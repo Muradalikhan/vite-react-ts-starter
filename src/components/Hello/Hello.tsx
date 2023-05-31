e49d2de3234b6a9cd6112fbb8ee1cb91d936fe92
Murad khan <murad.jaddoon@gmail.com>
2023-05-31 17:11:58 +0500

    Update Hello.tsx

--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,7 +1,7 @@
 import { Typography } from 'antd'
 
 const Hello = () => {
-  return <Typography.Title level={1}>Hello</Typography.Title>
+  return <Typography.Title level={1}>Hello world</Typography.Title>
 }
 
 export default Hello
